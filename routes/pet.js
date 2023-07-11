8d2ed76ef70f28ea7c3ae36abf3bd2fe9322507c
L-delara <ligia.delara15@gmail.com>
2023-07-10 21:39:10 -0400

    Added seeder with sample pets
    Everything currently working

--- routes/pet.js
+++ routes/pet.js
@@ -6,6 +6,7 @@ const {
   updatePetById,
   deletePetById,
 } = require("../controllers/Pet");
+const Pet = require("../models/Pet");
 
 // GET / get all Pets
 router.get("/", getAllPet);
@@ -23,3 +24,57 @@ router.put("/:id", updatePetById);
 router.delete("/:id", deletePetById);
 
 module.exports = router;
+
+//seed database
+router.get("/data/seed", async (req, res) => {
+  const data = [
+    {
+      petName: "Siobhan",
+      gender: "Female",
+      breed: "Tuxedo",
+      age: "7 months",
+      image:
+        "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/cat-placeholder.svg",
+      bio: "Loves tuna treats, laser pointers, and summoning otherworldly forces",
+    },
+    {
+      petName: "Boe",
+      gender: "Male",
+      breed: "Mastiff mix",
+      age: "3 years",
+      image:
+        "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/dog-placeholder.svg",
+      bio: "Loves cuddles and fortune-telling. Up for any adventure!",
+    },
+    {
+      petName: "Captain",
+      gender: "Male",
+      breed: "Lab mix",
+      age: "7 years",
+      image:
+        "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/dog-placeholder.svg",
+      bio: "Just vibin'. Will fetch anything. Ball. Shoes. Thor's Hammer.",
+    },
+    {
+      petName: "Tofu",
+      gender: "Female",
+      breed: "Calico",
+      age: "2 years",
+      image:
+        "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/cat-placeholder.svg",
+      bio: "Sleep all day! Party all night!",
+    },
+    {
+      petName: "Jim-Jam",
+      gender: "Male",
+      breed: "Heeler mix",
+      age: "10 years",
+      image:
+        "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/dog-placeholder.svg",
+      bio: "Nothing slows me down in my old age!",
+    },
+  ];
+
+  await Pet.insertMany(data);
+  res.status(303).redirect("/pets");
+});
